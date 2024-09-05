aeec7cb0d8ad22db2e10b5a9a0bec5326f932e2e
Benny Badenhorst <benny.badenhorst@water.co.nz>
2024-09-05 19:41:35 +1200

    Change Logo

--- .astro/types.d.ts
+++ .astro/types.d.ts
@@ -1,2 +1,342 @@
-/// <reference types="astro/client" />
-/// <reference path="astro/content.d.ts" />
+declare module 'astro:content' {
+	interface Render {
+		'.mdx': Promise<{
+			Content: import('astro').MarkdownInstance<{}>['Content'];
+			headings: import('astro').MarkdownHeading[];
+			remarkPluginFrontmatter: Record<string, any>;
+		}>;
+	}
+}
+
+declare module 'astro:content' {
+	interface Render {
+		'.md': Promise<{
+			Content: import('astro').MarkdownInstance<{}>['Content'];
+			headings: import('astro').MarkdownHeading[];
+			remarkPluginFrontmatter: Record<string, any>;
+		}>;
+	}
+}
+
+declare module 'astro:content' {
+	type Flatten<T> = T extends { [K: string]: infer U } ? U : never;
+
+	export type CollectionKey = keyof AnyEntryMap;
+	export type CollectionEntry<C extends CollectionKey> = Flatten<AnyEntryMap[C]>;
+
+	export type ContentCollectionKey = keyof ContentEntryMap;
+	export type DataCollectionKey = keyof DataEntryMap;
+
+	type AllValuesOf<T> = T extends any ? T[keyof T] : never;
+	type ValidContentEntrySlug<C extends keyof ContentEntryMap> = AllValuesOf<
+		ContentEntryMap[C]
+	>['slug'];
+
+	export function getEntryBySlug<
+		C extends keyof ContentEntryMap,
+		E extends ValidContentEntrySlug<C> | (string & {}),
+	>(
+		collection: C,
+		// Note that this has to accept a regular string too, for SSR
+		entrySlug: E
+	): E extends ValidContentEntrySlug<C>
+		? Promise<CollectionEntry<C>>
+		: Promise<CollectionEntry<C> | undefined>;
+
+	export function getDataEntryById<C extends keyof DataEntryMap, E extends keyof DataEntryMap[C]>(
+		collection: C,
+		entryId: E
+	): Promise<CollectionEntry<C>>;
+
+	export function getCollection<C extends keyof AnyEntryMap, E extends CollectionEntry<C>>(
+		collection: C,
+		filter?: (entry: CollectionEntry<C>) => entry is E
+	): Promise<E[]>;
+	export function getCollection<C extends keyof AnyEntryMap>(
+		collection: C,
+		filter?: (entry: CollectionEntry<C>) => unknown
+	): Promise<CollectionEntry<C>[]>;
+
+	export function getEntry<
+		C extends keyof ContentEntryMap,
+		E extends ValidContentEntrySlug<C> | (string & {}),
+	>(entry: {
+		collection: C;
+		slug: E;
+	}): E extends ValidContentEntrySlug<C>
+		? Promise<CollectionEntry<C>>
+		: Promise<CollectionEntry<C> | undefined>;
+	export function getEntry<
+		C extends keyof DataEntryMap,
+		E extends keyof DataEntryMap[C] | (string & {}),
+	>(entry: {
+		collection: C;
+		id: E;
+	}): E extends keyof DataEntryMap[C]
+		? Promise<DataEntryMap[C][E]>
+		: Promise<CollectionEntry<C> | undefined>;
+	export function getEntry<
+		C extends keyof ContentEntryMap,
+		E extends ValidContentEntrySlug<C> | (string & {}),
+	>(
+		collection: C,
+		slug: E
+	): E extends ValidContentEntrySlug<C>
+		? Promise<CollectionEntry<C>>
+		: Promise<CollectionEntry<C> | undefined>;
+	export function getEntry<
+		C extends keyof DataEntryMap,
+		E extends keyof DataEntryMap[C] | (string & {}),
+	>(
+		collection: C,
+		id: E
+	): E extends keyof DataEntryMap[C]
+		? Promise<DataEntryMap[C][E]>
+		: Promise<CollectionEntry<C> | undefined>;
+
+	/** Resolve an array of entry references from the same collection */
+	export function getEntries<C extends keyof ContentEntryMap>(
+		entries: {
+			collection: C;
+			slug: ValidContentEntrySlug<C>;
+		}[]
+	): Promise<CollectionEntry<C>[]>;
+	export function getEntries<C extends keyof DataEntryMap>(
+		entries: {
+			collection: C;
+			id: keyof DataEntryMap[C];
+		}[]
+	): Promise<CollectionEntry<C>[]>;
+
+	export function reference<C extends keyof AnyEntryMap>(
+		collection: C
+	): import('astro/zod').ZodEffects<
+		import('astro/zod').ZodString,
+		C extends keyof ContentEntryMap
+			? {
+					collection: C;
+					slug: ValidContentEntrySlug<C>;
+				}
+			: {
+					collection: C;
+					id: keyof DataEntryMap[C];
+				}
+	>;
+	// Allow generic `string` to avoid excessive type errors in the config
+	// if `dev` is not running to update as you edit.
+	// Invalid collection names will be caught at build time.
+	export function reference<C extends string>(
+		collection: C
+	): import('astro/zod').ZodEffects<import('astro/zod').ZodString, never>;
+
+	type ReturnTypeOrOriginal<T> = T extends (...args: any[]) => infer R ? R : T;
+	type InferEntrySchema<C extends keyof AnyEntryMap> = import('astro/zod').infer<
+		ReturnTypeOrOriginal<Required<ContentConfig['collections'][C]>['schema']>
+	>;
+
+	type ContentEntryMap = {
+		"about": {
+"index.mdx": {
+	id: "index.mdx";
+  slug: "index";
+  body: string;
+  collection: "about";
+  data: any
+} & { render(): Render[".mdx"] };
+};
+"authors": {
+"-index.md": {
+	id: "-index.md";
+  slug: "-index";
+  body: string;
+  collection: "authors";
+  data: InferEntrySchema<"authors">
+} & { render(): Render[".md"] };
+"ben.mdx": {
+	id: "ben.mdx";
+  slug: "ben";
+  body: string;
+  collection: "authors";
+  data: InferEntrySchema<"authors">
+} & { render(): Render[".mdx"] };
+};
+"pages": {
+"404.md": {
+	id: "404.md";
+  slug: "404";
+  body: string;
+  collection: "pages";
+  data: InferEntrySchema<"pages">
+} & { render(): Render[".md"] };
+"bmi.md": {
+	id: "bmi.md";
+  slug: "bmi";
+  body: string;
+  collection: "pages";
+  data: InferEntrySchema<"pages">
+} & { render(): Render[".md"] };
+"contact.md": {
+	id: "contact.md";
+  slug: "contact";
+  body: string;
+  collection: "pages";
+  data: InferEntrySchema<"pages">
+} & { render(): Render[".md"] };
+"elements.mdx": {
+	id: "elements.mdx";
+  slug: "elements";
+  body: string;
+  collection: "pages";
+  data: InferEntrySchema<"pages">
+} & { render(): Render[".mdx"] };
+"gki.md": {
+	id: "gki.md";
+  slug: "gki";
+  body: string;
+  collection: "pages";
+  data: InferEntrySchema<"pages">
+} & { render(): Render[".md"] };
+"privacy-policy.md": {
+	id: "privacy-policy.md";
+  slug: "privacy-policy";
+  body: string;
+  collection: "pages";
+  data: InferEntrySchema<"pages">
+} & { render(): Render[".md"] };
+};
+"posts": {
+"-index.md": {
+	id: "-index.md";
+  slug: "-index";
+  body: string;
+  collection: "posts";
+  data: InferEntrySchema<"posts">
+} & { render(): Render[".md"] };
+"120-hours-water-fast-journey.md": {
+	id: "120-hours-water-fast-journey.md";
+  slug: "120-hours-water-fast-journey";
+  body: string;
+  collection: "posts";
+  data: InferEntrySchema<"posts">
+} & { render(): Render[".md"] };
+"can-you-have-beer-or-wine-on-keto-the-intoxicating-truth-about-keto-and-alcohol.md": {
+	id: "can-you-have-beer-or-wine-on-keto-the-intoxicating-truth-about-keto-and-alcohol.md";
+  slug: "can-you-have-beer-or-wine-on-keto-the-intoxicating-truth-about-keto-and-alcohol";
+  body: string;
+  collection: "posts";
+  data: InferEntrySchema<"posts">
+} & { render(): Render[".md"] };
+"easy-steps-for-transitioning-from-a-high-to-a-low-carb-diet.md": {
+	id: "easy-steps-for-transitioning-from-a-high-to-a-low-carb-diet.md";
+  slug: "easy-steps-for-transitioning-from-a-high-to-a-low-carb-diet";
+  body: string;
+  collection: "posts";
+  data: InferEntrySchema<"posts">
+} & { render(): Render[".md"] };
+"getting-to-know-autophagy-what-it-is-why-it-matters-how-to-try-it.md": {
+	id: "getting-to-know-autophagy-what-it-is-why-it-matters-how-to-try-it.md";
+  slug: "getting-to-know-autophagy-what-it-is-why-it-matters-how-to-try-it";
+  body: string;
+  collection: "posts";
+  data: InferEntrySchema<"posts">
+} & { render(): Render[".md"] };
+"keto-food-faves-whats-in-out-booze-too.md": {
+	id: "keto-food-faves-whats-in-out-booze-too.md";
+  slug: "keto-food-faves-whats-in-out-booze-too";
+  body: string;
+  collection: "posts";
+  data: InferEntrySchema<"posts">
+} & { render(): Render[".md"] };
+"keto-tales-avocados-donuts-and-bacon.md": {
+	id: "keto-tales-avocados-donuts-and-bacon.md";
+  slug: "keto-tales-avocados-donuts-and-bacon";
+  body: string;
+  collection: "posts";
+  data: InferEntrySchema<"posts">
+} & { render(): Render[".md"] };
+"low-carb-keto-smoothie.md": {
+	id: "low-carb-keto-smoothie.md";
+  slug: "low-carb-keto-smoothie";
+  body: string;
+  collection: "posts";
+  data: InferEntrySchema<"posts">
+} & { render(): Render[".md"] };
+"low-carb-sprout-about-town-4-quick-recipes.md": {
+	id: "low-carb-sprout-about-town-4-quick-recipes.md";
+  slug: "low-carb-sprout-about-town-4-quick-recipes";
+  body: string;
+  collection: "posts";
+  data: InferEntrySchema<"posts">
+} & { render(): Render[".md"] };
+"references-research.md": {
+	id: "references-research.md";
+  slug: "references-research";
+  body: string;
+  collection: "posts";
+  data: InferEntrySchema<"posts">
+} & { render(): Render[".md"] };
+"taming-stubborn-belly-fat-guide-battling-visceral-fat.md": {
+	id: "taming-stubborn-belly-fat-guide-battling-visceral-fat.md";
+  slug: "taming-stubborn-belly-fat-guide-battling-visceral-fat";
+  body: string;
+  collection: "posts";
+  data: InferEntrySchema<"posts">
+} & { render(): Render[".md"] };
+"the-great-beer-debate-sorting-fact-from-froth-trans-tasman-style.md": {
+	id: "the-great-beer-debate-sorting-fact-from-froth-trans-tasman-style.md";
+  slug: "the-great-beer-debate-sorting-fact-from-froth-trans-tasman-style";
+  body: string;
+  collection: "posts";
+  data: InferEntrySchema<"posts">
+} & { render(): Render[".md"] };
+"the-relationship-between-low-carb-diets-and-fasting-impacts-on-insulin-and-cortisol.md": {
+	id: "the-relationship-between-low-carb-diets-and-fasting-impacts-on-insulin-and-cortisol.md";
+  slug: "the-relationship-between-low-carb-diets-and-fasting-impacts-on-insulin-and-cortisol";
+  body: string;
+  collection: "posts";
+  data: InferEntrySchema<"posts">
+} & { render(): Render[".md"] };
+"what-s-the-buzz-about-mct-oil-benefits-and-everyday-uses.md": {
+	id: "what-s-the-buzz-about-mct-oil-benefits-and-everyday-uses.md";
+  slug: "what-s-the-buzz-about-mct-oil-benefits-and-everyday-uses";
+  body: string;
+  collection: "posts";
+  data: InferEntrySchema<"posts">
+} & { render(): Render[".md"] };
+"world-of-intermittent-fasting.md": {
+	id: "world-of-intermittent-fasting.md";
+  slug: "world-of-intermittent-fasting";
+  body: string;
+  collection: "posts";
+  data: InferEntrySchema<"posts">
+} & { render(): Render[".md"] };
+};
+"products": {
+"index.mdx": {
+	id: "index.mdx";
+  slug: "index";
+  body: string;
+  collection: "products";
+  data: any
+} & { render(): Render[".mdx"] };
+};
+"videos": {
+"index.mdx": {
+	id: "index.mdx";
+  slug: "index";
+  body: string;
+  collection: "videos";
+  data: any
+} & { render(): Render[".mdx"] };
+};
+
+	};
+
+	type DataEntryMap = {
+		
+	};
+
+	type AnyEntryMap = ContentEntryMap & DataEntryMap;
+
+	export type ContentConfig = typeof import("../src/content/config.js");
+}
